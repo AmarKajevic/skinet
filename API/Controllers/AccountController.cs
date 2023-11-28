51a907631d89848f6b5f378eb79ff0015f381e4a
Amar <kajevic18@gmail>
2023-11-28 13:48:52 +0100

    added login and register UI for app

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -28,7 +28,7 @@ namespace API.Controllers
             _tokenService = tokenService;
             _signInManager = signInManager;
             _userManager = userManager;
-        }
+        }  
         [HttpGet]
         [Authorize]
         public async Task<ActionResult<UserDto>> GetCurrentUser(){
@@ -93,6 +93,11 @@ namespace API.Controllers
         [HttpPost("register")]
         public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
         {
+            if(CheckEmailExistAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse{Errors = new[]
+                {"Email address is in use"}});
+            }
             var user = new AppUser{
                 Displayname = registerDto.Displayname,
                 Email = registerDto.Email,
---
+++ client/src/app/shared/models/user.ts
@@ -0,0 +1,13 @@
+export interface User{
+    email: string;
+    displayname: string;
+    token: string;
+}
+export interface Addres {
+    firstName: string;
+    lastName: string;
+    street: string;
+    city: string;
+    state: string;
+    zipcode: string;
+}
